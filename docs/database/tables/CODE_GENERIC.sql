995f4c15ec01927aef7662403085371a5c7cdaa8
Ekaterina Voronina <vea.kat@gmail.com>
2017-03-17 16:16:13 +0300

    SEDC-106: add SYSTEM_VARIABLE table

--- docs/database/tables/CODE_GENERIC.sql
+++ docs/database/tables/CODE_GENERIC.sql
@@ -1,12 +1,12 @@
 CREATE TABLE CODE_GENERIC (
-     CG_ID          NUMERIC(18) NOT NULL,
-     TYPE           VARCHAR(20) NOT NULL,
-     NAME           VARCHAR(20) NOT NULL,
-     DESCRIPTION    VARCHAR(40) NOT NULL,
-     ACTIVE_FLAG    CHAR(1) DEFAULT 'Y' NOT NULL,
+                                CG_ID          NUMERIC(18) NOT NULL,
+                                TYPE           VARCHAR(20) NOT NULL,
+                                NAME           VARCHAR(20) NOT NULL,
+                                DESCRIPTION    VARCHAR(40) NOT NULL,
+                                ACTIVE_FLAG    CHAR(1) DEFAULT 'Y' NOT NULL,
 
-     CONSTRAINT IPK_CODE_GENERIC PRIMARY KEY (CG_ID) DEFAULT nextval('S_CODE_GENERIC_PK')
-) TABLESPACE GENERAL_INDEX;
+                                CONSTRAINT IPK_CODE_GENERIC PRIMARY KEY (CG_ID) DEFAULT nextval('S_CODE_GENERIC_PK')
+                           ) TABLESPACE GENERAL_INDEX;
 
 CREATE UNIQUE INDEX IUK_CODE_GENERIC_NAME ON CODE_GENERIC (TYPE, NAME) TABLESPACE GENERAL_INDEX;
 
