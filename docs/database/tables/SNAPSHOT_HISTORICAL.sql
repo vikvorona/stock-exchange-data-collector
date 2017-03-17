624f5354c8b578a0ff5d7f6ac9ba549c1ec1bab9
Ekaterina Voronina <vea.kat@gmail.com>
2017-03-17 16:32:05 +0300

    SEDC-113: add SNAPSHOT_HISTORICAL table

--- docs/database/tables/SNAPSHOT_HISTORICAL.sql
+++ docs/database/tables/SNAPSHOT_HISTORICAL.sql
@@ -1,3 +1,18 @@
 CREATE TABLE SNAPSHOT_HISTORICAL (
+                             SCEI_ID         NUMERIC(18) NOT NULL,
+                             BUSINESS_DATE   NUMERIC(18) NOT NULL,
+                             SYM_ID          NUMERIC(18) NOT NULL,
+                             PER             NUMERIC(18) NOT NULL,
+                             DATE            NUMERIC(18) NOT NULL,
+                             TIME            NUMERIC(18) NOT NULL,
+                             OPEN            NUMERIC(18) NOT NULL,
+                             HIGH            NUMERIC(18) NOT NULL,
+                             LOW             NUMERIC(18) NOT NULL,
+                             CLOSE           NUMERIC(18) NOT NULL,
+                             VOLUME          NUMERIC(18) NOT NULL,
+                             ACTIVE_FLAG     CHAR(1) DEFAULT 'Y' NOT NULL,
 
-) TABLESPACE GENERAL_INDEX;
+CONSTRAINT IPK_SNAPSHOT_HISTORICAL PRIMARY KEY (SCEI_ID) DEFAULT nextval('S_SNAPSHOT_HISTORICAL_PK')
+                           ) TABLESPACE GENERAL_INDEX;
+
+CREATE UNIQUE INDEX IUK_SNAPSHOT_HISTORICAL_NAME ON SNAPSHOT_HISTORICAL (TYPE, NAME) TABLESPACE GENERAL_INDEX;
