4bc92db765df596815ccaa46bf486bd4c28c3ae5
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2017-03-18 22:43:18 +0300

    comment zabbix config. need to find out how pass db host dynamical

--- docs/docker/install-containers.sh
+++ docs/docker/install-containers.sh
@@ -35,48 +35,45 @@ POSTGRES_USER="postgres"
 POSTGRES_PASSWORD="postgres"
 POSTGRES_DB="postgres"
 
-docker run --name postgres-server -t \
+docker run -d --name postgres-server -t \
       -p ${DB_SERVER_PORT}:5432 \
       -e POSTGRES_USER=${POSTGRES_USER} \
       -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
       -e POSTGRES_DB=${POSTGRES_DB} \
       -v /var/run/docker.sock:/var/run/docker.sock \
-      -d postgres:latest
+      postgres
 
 echo "Postgres installed in Docker, port ${DB_SERVER_PORT}"
 
 # Zabbix parameters
-ZBX_SERVER_NAME="SEDC"
-DB_SERVER_HOST=${HOST_IP}
-
-docker run --name zabbix-snmptraps -t \
-      -v /zbx_instance/snmptraps:/var/lib/zabbix/snmptraps:rw \
-      -v /var/lib/zabbix/mibs:/usr/share/snmp/mibs:ro \
-      -p 162:162/udp \
-      -d zabbix/zabbix-snmptraps:latest
-
-docker run --name zabbix-server-pgsql -t \
-      -e DB_SERVER_HOST=${DB_SERVER_HOST} \
-      -e DB_SERVER_PORT=${DB_SERVER_PORT} \
-      -e POSTGRES_USER=${POSTGRES_USER} \
-      -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-      -e POSTGRES_DB=${POSTGRES_DB} \
-      -e ZBX_ENABLE_SNMP_TRAPS="true" \
-      --link postgres-server:postgres \
-      -p 10051:10051 \
-      --volumes-from zabbix-snmptraps \
-      -d zabbix/zabbix-server-pgsql:latest
-
-docker run --name zabbix-web-nginx-pgsql -t \
-      -e DB_SERVER_HOST=${DB_SERVER_HOST} \
-      -e DB_SERVER_PORT=${DB_SERVER_PORT} \
-      -e POSTGRES_USER=${POSTGRES_USER} \
-      -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-      -e POSTGRES_DB=${POSTGRES_DB} \
-      --link postgres-server:postgres \
-      --link zabbix-server-pgsql:zabbix-server \
-      -p 8020:8020 \
-      -v /etc/ssl/nginx:/etc/ssl/nginx:ro \
-      -d zabbix/zabbix-web-nginx-pgsql:latest
-
-echo "Grafana installed in Docker, port 8010"
+#ZBX_SERVER_NAME="SEDC"
+#DB_SERVER_HOST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgres-server)
+#
+#docker run --name zabbix-snmptraps -t \
+#      -v /zbx_instance/snmptraps:/var/lib/zabbix/snmptraps:rw \
+#      -v /var/lib/zabbix/mibs:/usr/share/snmp/mibs:ro \
+#      -p 162:162/udp \
+#      -d zabbix/zabbix-snmptraps
+#
+#docker run --name zabbix-server-pgsql -t \
+#      -e DB_SERVER_HOST=${DB_SERVER_HOST} \
+#      -e POSTGRES_USER=${POSTGRES_USER} \
+#      -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+#      -e ZBX_ENABLE_SNMP_TRAPS="true" \
+#      --link postgres-server:postgres \
+#      -p 10051:10051 \
+#      --volumes-from zabbix-snmptraps \
+#      -d zabbix/zabbix-server-pgsql
+#
+#docker run --name zabbix-web-apache-pgsql -t \
+#      -e DB_SERVER_HOST=${DB_SERVER_HOST} \
+#      -e POSTGRES_USER=${POSTGRES_USER} \
+#      -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+#      --link postgres-server:postgres \
+#      --link zabbix-server-pgsql:zabbix-server \
+#      -p 8020:80 \
+#      -p 8021:443 \
+#      -v /etc/ssl/nginx:/etc/ssl/nginx:ro \
+#      -d zabbix/zabbix-web-apache-pgsql
+#
+#echo "Zabbix installed in Docker, port 8010"
