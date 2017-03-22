964986535caeb0af3ecb936712f1e843d077b9a0
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2017-03-22 22:55:18 +0300

    fix Codacy issues: - Avoid unused local variables - System.exit() should not be used in J2EE/JEE apps

--- sedc-core/src/main/java/com/sedc/collectors/yahoo/industry/YahooIndustryApplication.java
+++ sedc-core/src/main/java/com/sedc/collectors/yahoo/industry/YahooIndustryApplication.java
@@ -1,45 +1,10 @@
 package com.sedc.collectors.yahoo.industry;
 
-import org.apache.log4j.Logger;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobExecutionException;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.batch.core.launch.support.CommandLineJobRunner;
 
 public class YahooIndustryApplication {
 
-    private static final Logger LOG = Logger.getLogger(YahooIndustryApplication.class);
-
-    @Autowired
-    private JobLauncher jobLauncher;
-
-    @Autowired
-    private Job job;
-
-    public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring/batch/jobs/yahoo-industry-load-job.xml");
-        YahooIndustryApplication app = (YahooIndustryApplication) context.getBean("app");
-        app.run();
-    }
-
-    private void run() {
-        try {
-            JobExecution execution = jobLauncher.run(job, new JobParameters());
-        } catch (JobExecutionException e) {
-            LOG.error(e.getMessage(), e);
-            System.exit(1);
-        }
-    }
-
-    public void setJobLauncher(JobLauncher jobLauncher) {
-        this.jobLauncher = jobLauncher;
-    }
-
-    public void setJob(Job job) {
-        this.job = job;
+    public static void main(String[] args) throws Exception {
+        CommandLineJobRunner.main(new String[]{"spring/batch/jobs/yahoo-industry-load-job.xml","yahoo-industry-load-job"});
     }
 }
