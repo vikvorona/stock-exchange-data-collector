be0d0acd188009642baecd44defeb05e6ccdb19d
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2017-03-26 16:43:51 +0300

    SEDC-153: add SymbolLoadApplication

---
+++ sedc-core/src/main/java/com/sedc/symbolload/SymbolLoadApplication.java
@@ -0,0 +1,27 @@
+package com.sedc.symbolload;
+
+import org.apache.log4j.Logger;
+import org.springframework.batch.core.launch.support.CommandLineJobRunner;
+
+public class SymbolLoadApplication {
+
+    private static final Logger LOG = Logger.getLogger(SymbolLoadApplication.class);
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1){
+            LOG.error("<EXCHANGE> parameter is required");
+            System.exit(1);
+        }
+
+        String exchange = args[0];
+
+        switch (exchange){
+            case "MICEX":
+                CommandLineJobRunner.main(new String[]{"spring/batch/jobs/symbol-load-micex-job.xml", "symbol-load-micex-job"});
+                break;
+            default:
+                LOG.error(exchange + " is not realized");
+                System.exit(1);
+        }
+    }
+}
