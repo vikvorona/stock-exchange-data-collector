75ea7a7a46cc250d5aaa1bcf8924375d59c4fb7c
Yana-Guseva <9301YanaGuseva@gmail.com>
2017-03-26 19:40:28 +0300

    inject dates to FinamResourceGenerator

--- sedc-core/src/main/java/com/sedc/collectors/finam/historical/FinamResourceGenerator.java
+++ sedc-core/src/main/java/com/sedc/collectors/finam/historical/FinamResourceGenerator.java
@@ -6,6 +6,8 @@ import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.UrlResource;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -19,18 +21,54 @@ public class FinamResourceGenerator implements ItemWriter<String> {
     private String period;
     @Autowired
     private String region;
+    @Autowired
+    private Date dateFrom;
+    @Autowired
+    private Date dateTo;
+
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
     public void write(List<? extends String> items) throws Exception {
 
+        String startDate = dateFormat.format(dateFrom);
+        String endDate = dateFormat.format(dateTo);
+        String startDay = getDay(dateFrom);
+        String startMonth = getMonth(dateFrom);
+        String startYear = getYear(dateFrom);
+        String endDay = getDay(dateTo);
+        String endMonth = getMonth(dateTo);
+        String endYear = getYear(dateTo);
+
         for (String symbol : items) {
 
-            String queryParams[] = {"1", "16842", symbol, "0", "24", "1", "2017", "24.02.2017", "1", "2", "2017",
-                    "01.03.2017", "5", symbol + new Date(), ".txt", symbol, "1", "1", "1", "on", "1", "1", "1", "1"};
+            String queryParams[] = {"1", "16842", symbol, "0", startDay, startMonth, startYear, startDate, endDay, endMonth, endYear,
+                    endDate, "5", symbol + new Date(), ".txt", symbol, "1", "1", "1", "on", "1", "1", "1", "1"};
 
             UrlResource resource = new UrlResource(FinamUtils.buildUrl(queryParams));
             LOG.debug("Generated resource: " + resource);
             urls.add(resource);
         }
+    }
+
+    private String getYear(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int year = cal.get(Calendar.YEAR);
+        return String.valueOf(year);
+    }
+
+    private String getMonth(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int month = cal.get(Calendar.MONTH);
+        return String.valueOf(month);
+    }
+
+    private String getDay(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return String.valueOf(day);
     }
 
     public void setUrls(List<UrlResource> urls) {
@@ -43,5 +81,13 @@ public class FinamResourceGenerator implements ItemWriter<String> {
 
     public void setRegion(String region) {
         this.region = region;
+    }
+
+    public void setDateFrom(Date dateFrom) {
+        this.dateFrom = dateFrom;
+    }
+
+    public void setDateTo(Date dateTo) {
+        this.dateTo = dateTo;
     }
 }
