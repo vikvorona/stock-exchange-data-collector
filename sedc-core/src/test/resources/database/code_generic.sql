75971d83abd0e99fd976e42643900eed06152ba8
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2017-03-26 16:41:37 +0300

    SEDC-153: make code generic load

---
+++ sedc-core/src/test/resources/database/code_generic.sql
@@ -0,0 +1,5 @@
+INSERT INTO code_generic VALUES(NEXT VALUE FOR S_CODE_GENERIC_PK, 'REGION', 'APAC', 'Asia pacific region', 'Y');
+
+INSERT INTO code_generic VALUES(NEXT VALUE FOR S_CODE_GENERIC_PK, 'REGION', 'EMEA', 'European region', 'Y');
+
+INSERT INTO code_generic VALUES(NEXT VALUE FOR S_CODE_GENERIC_PK, 'REGION', 'NAM', 'American region', 'Y');
