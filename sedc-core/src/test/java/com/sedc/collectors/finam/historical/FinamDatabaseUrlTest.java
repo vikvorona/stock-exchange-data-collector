62529bbcfa54cad6ca98084c688c2df2c3f290d1
Yana-Guseva <9301YanaGuseva@gmail.com>
2017-03-26 17:08:44 +0300

    refactor finam tests

--- sedc-core/src/test/java/com/sedc/collectors/finam/historical/FinamDatabaseUrlTest.java
+++ sedc-core/src/test/java/com/sedc/collectors/finam/historical/FinamDatabaseUrlTest.java
@@ -15,33 +15,32 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("/spring/batch/jobs/finam-job-test.xml")
-public class FinamSimpleTest {
+@ContextConfiguration("/spring/batch/jobs/finam-database-file-test.xml")
+public class FinamDatabaseFileTest {
 
-    private static final Logger LOG = Logger.getLogger(FinamSimpleTest.class);
+    private static final Logger LOG = Logger.getLogger(FinamDatabaseFileTest.class);
 
     @Autowired
     private JobLauncher jobLauncher;
     @Autowired
-    private FlatFileItemReader reader;
-    @Autowired
     private Job job;
+
+    public FinamDatabaseFileTest() {
+    }
 
     @Test
     public void testFile() throws Exception {
 
-        reader.setResource(new ClassPathResource("com/sedc/collectors/finam/historical/sample-data.csv"));
-
         JobExecution execution = jobLauncher.run(job, new JobParameters());
         LOG.info("Exit Status : " + execution.getStatus());
     }
 
-    @Test
-    public void testUrl() throws Exception {
-
-        reader.setResource(new UrlResource("http://export.finam.ru/GAZP_170224_170301.txt?market=1&em=16842&code=GAZP&apply=0&df=24&mf=1&yf=2017&from=24.02.2017&dt=1&mt=2&yt=2017&to=01.03.2017&p=5&f=GAZP_170224_170301&e=.txt&cn=GAZP&dtf=1&tmf=1&MSOR=1&mstime=on&mstimever=1&sep=1&sep2=1&datf=1"));
-
-        JobExecution execution = jobLauncher.run(job, new JobParameters());
-        LOG.info("Exit Status : " + execution.getStatus());
-    }
+//    @Test
+//    public void testUrl() throws Exception {
+//
+//        reader.setResource(new UrlResource("http://export.finam.ru/GAZP_170224_170301.txt?market=1&em=16842&code=GAZP&apply=0&df=24&mf=1&yf=2017&from=24.02.2017&dt=1&mt=2&yt=2017&to=01.03.2017&p=5&f=GAZP_170224_170301&e=.txt&cn=GAZP&dtf=1&tmf=1&MSOR=1&mstime=on&mstimever=1&sep=1&sep2=1&datf=1"));
+//
+//        JobExecution execution = jobLauncher.run(job, new JobParameters());
+//        LOG.info("Exit Status : " + execution.getStatus());
+//    }
 }
---
+++ sedc-core/src/test/java/com/sedc/collectors/finam/historical/FinamSimpleUrlTest.java
@@ -0,0 +1,7 @@
+package com.sedc.collectors.finam.historical;
+
+/**
+ * Created by Yana on 26.03.2017.
+ */
+public class FinamSimpleUrlTest {
+}
