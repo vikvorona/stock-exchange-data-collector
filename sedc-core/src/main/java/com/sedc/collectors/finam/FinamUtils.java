09fdc7a825efd893a1d57b040ae9a406d1a163c1
Yana-Guseva <9301YanaGuseva@gmail.com>
2017-03-28 00:46:26 +0300

    move params to utils class

--- sedc-core/src/main/java/com/sedc/collectors/finam/FinamUtils.java
+++ sedc-core/src/main/java/com/sedc/collectors/finam/FinamUtils.java
@@ -3,30 +3,62 @@ package com.sedc.collectors.finam;
 import org.apache.http.client.utils.URIBuilder;
 
 import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class FinamUtils {
 
-    public static String buildUrl(String params[]) throws URISyntaxException {
-        URIBuilder uri = new URIBuilder("http://export.finam.ru/out");
-        uri.addParameter("market", params[0]);
-        uri.addParameter("em", params[1]);
-        uri.addParameter("code", params[2]);
-        uri.addParameter("apply", params[3]);
-        uri.addParameter("df", params[4]);
-        uri.addParameter("mf", params[5]);
-        uri.addParameter("yf", params[6]);
-        uri.addParameter("from", params[7]);
-        uri.addParameter("dt", params[8]);
-        uri.addParameter("mt", params[9]);
-        uri.addParameter("yt", params[10]);
-        uri.addParameter("to", params[11]);
-        uri.addParameter("p", params[12]);
-        uri.addParameter("cn", params[13]);
-        uri.addParameter("dtf", params[14]);
-        uri.addParameter("tmf", params[15]);
-        uri.addParameter("sep", params[16]);
-        uri.addParameter("sep2", params[17]);
-        uri.addParameter("datf", params[18]);
+    private static final String BASE_URL = "http://export.finam.ru/out";
+    private static final String MARKET = "1";
+    private static final String TICKER_FLAG = "1";
+    private static final String SEPARATOR1 = "1";
+    private static final String SEPARATOR2 = "1";
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+    public static String buildUrl(String symbol, Date dateFrom, Date dateTo) throws URISyntaxException {
+        URIBuilder uri = new URIBuilder(BASE_URL);
+        uri.addParameter("market", MARKET);
+        uri.addParameter("em", "16842");
+        uri.addParameter("code", symbol);
+        uri.addParameter("apply", "0");
+        uri.addParameter("df", getDay(dateFrom));
+        uri.addParameter("mf", getMonth(dateFrom));
+        uri.addParameter("yf", getYear(dateFrom));
+        uri.addParameter("from", dateFormat.format(dateFrom));
+        uri.addParameter("dt", getDay(dateTo));
+        uri.addParameter("mt", getMonth(dateTo));
+        uri.addParameter("yt", getYear(dateTo));
+        uri.addParameter("to", dateFormat.format(dateTo));
+        uri.addParameter("p", "5");
+        uri.addParameter("cn", symbol);
+        uri.addParameter("dtf", "1");
+        uri.addParameter("tmf", "1");
+        uri.addParameter("sep", SEPARATOR1);
+        uri.addParameter("sep2", SEPARATOR2);
+        uri.addParameter("datf", TICKER_FLAG);
         return uri.build().toString();
+    }
+
+    private static String getYear(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int year = cal.get(Calendar.YEAR);
+        return String.valueOf(year);
+    }
+
+    private static String getMonth(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int month = cal.get(Calendar.MONTH);
+        return String.valueOf(month);
+    }
+
+    private static String getDay(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return String.valueOf(day);
     }
 }
