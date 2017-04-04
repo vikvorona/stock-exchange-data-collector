6abfeb4112ad1c5b2cf0a7208e75d690659ae496
Oleg Shulykov <olegshulyakov@users.noreply.github.com>
2017-04-04 15:18:31 +0300

    code review final

--- sedc-core/src/main/java/com/sedc/collectors/yahoo/historical/YahooHistoricalApplication.java
+++ sedc-core/src/main/java/com/sedc/collectors/yahoo/historical/YahooHistoricalApplication.java
@@ -4,6 +4,6 @@ import org.springframework.batch.core.launch.support.CommandLineJobRunner;
 
 public class YahooHistoricalApplication {
     public static void main(String[] args) throws Exception {
-        CommandLineJobRunner.main(new String[]{"spring/batch/jobs/yahoo/yahoo-historical-load-job.xml", "yahooHistoricalLoadJob"});
+        CommandLineJobRunner.main(new String[]{"spring/batch/jobs/yahoo/yahoo-historical-load-job.xml", "yahooHistoricalLoadJob", "region=YAHOO_HISTORY"});
     }
 }
--- sedc-core/src/main/java/com/sedc/collectors/yahoo/util/YahooResourceGenerator.java
+++ sedc-core/src/main/java/com/sedc/collectors/yahoo/util/YahooResourceGenerator.java
@@ -1,93 +1,50 @@
 package com.sedc.collectors.yahoo.util;
 
-import com.sedc.collectors.finam.FinamUtils;
-import com.sedc.collectors.yahoo.historical.model.YahooHistoricalRecord;
-import com.sedc.collectors.yahoo.quote.model.YahooQuoteRecord;
-import com.sedc.managers.SymbolManager;
-import com.sedc.managers.SystemVariableManager;
-import com.sedc.managers.SystemVariables;
 import org.apache.log4j.Logger;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.core.io.UrlResource;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
-import java.net.MalformedURLException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
 
 public class YahooResourceGenerator implements ItemWriter<String> {
 
     private static final Logger LOG = Logger.getLogger(YahooResourceGenerator.class);
-    private static final String QUOTE_ROOT_ELEMENT = "quote";
 
     private List<UrlResource> urls;
-    private String period;
-    private Date dateFrom;
-    private Date dateTo;
 
     private String sourceEngineName;
-    private SymbolManager symbolManager;
-    private SystemVariableManager systemVariableManager;
 
-    public YahooResourceGenerator(String sourceEngineName,
-                                  SymbolManager symbolManager,
-                                  SystemVariableManager systemVariableManager) {
-        this.sourceEngineName = sourceEngineName;
-        this.symbolManager = symbolManager;
-        this.systemVariableManager = systemVariableManager;
-    }
-
-    public ItemReader getHistoricalReader() throws MalformedURLException {
-        List<String> symbols = symbolManager.getStringSymbolsBySource(sourceEngineName);
-        LocalDate startDate = LocalDate.parse(systemVariableManager.getSystemVariable(SystemVariables.LAST_UPDATE_DATE),
-                DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-        LocalDate endDate = LocalDate.now();
-        UrlResource resource = YahooResourceHelper.getHistoricalDataResource(symbols, startDate, endDate);
-        return getStaxReader(QUOTE_ROOT_ELEMENT, YahooHistoricalRecord.class, resource);
-    }
-
-    public ItemReader getQuoteReader() throws MalformedURLException {
-        List<String> symbols = symbolManager.getStringSymbolsBySource(sourceEngineName);
-        UrlResource resource = YahooResourceHelper.getQuoteResource(symbols);
-        return getStaxReader(QUOTE_ROOT_ELEMENT, YahooQuoteRecord.class, resource);
-    }
-
-    private ItemReader getStaxReader(String fragmentRootElementName, Class classToBeBound, UrlResource resource) {
-        Jaxb2Marshaller unmarshaller = new Jaxb2Marshaller();
-        unmarshaller.setClassesToBeBound(classToBeBound);
-        StaxEventItemReader reader = new StaxEventItemReader();
-        reader.setUnmarshaller(unmarshaller);
-        reader.setFragmentRootElementName(fragmentRootElementName);
-        reader.setResource(resource);
-        return reader;
-    }
+    private LocalDate startDate;
 
     @Override
     public void write(List<? extends String> list) throws Exception {
-        for (String symbol : list) {
-            UrlResource resource = new UrlResource(FinamUtils.buildUrl(symbol, dateFrom, dateTo, period));
-            LOG.debug("Generated resource: " + resource);
-            urls.add(resource);
+        UrlResource resource;
+
+        switch (sourceEngineName) {
+            case "YAHOO_HISTORICAL":
+                resource = YahooResourceHelper.getHistoricalDataResource((List<String>) list, startDate, LocalDate.now());
+                break;
+            case "YAHOO_QUOTE":
+                resource = YahooResourceHelper.getQuoteResource((List<String>) list);
+
+                break;
+            default:
+                throw new UnsupportedOperationException("Source engine " + sourceEngineName + " is unimplemented");
         }
+        LOG.debug("Generated resource: " + resource);
+        urls.add(resource);
     }
 
     public void setUrls(List<UrlResource> urls) {
         this.urls = urls;
     }
 
-    public void setPeriod(String period) {
-        this.period = period;
+    public void setSourceEngineName(String sourceEngineName) {
+        this.sourceEngineName = sourceEngineName;
     }
 
-    public void setDateFrom(Date dateFrom) {
-        this.dateFrom = dateFrom;
-    }
-
-    public void setDateTo(Date dateTo) {
-        this.dateTo = dateTo;
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
     }
 }
