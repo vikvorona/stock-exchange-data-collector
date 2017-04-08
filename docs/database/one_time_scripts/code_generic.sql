888a9403bd0bd9b38dbb67d1f6c329c4843c8cb6
Oleg <oleg@example.com>
2017-04-08 17:53:06 +0300

    add source states

--- docs/database/one_time_scripts/code_generic.sql
+++ docs/database/one_time_scripts/code_generic.sql
@@ -1,5 +1,11 @@
 INSERT INTO code_generic VALUES(nextval('s_code_generic_pk'), 'REGION', 'APAC', 'Asia pacific region', 'Y');
-
 INSERT INTO code_generic VALUES(nextval('s_code_generic_pk'), 'REGION', 'EMEA', 'European region', 'Y');
-
 INSERT INTO code_generic VALUES(nextval('s_code_generic_pk'), 'REGION', 'NAM', 'American region', 'Y');
+
+INSERT INTO code_generic VALUES(nextval('s_code_generic_pk'), 'SOURCE_STATE', '0', 'INITIAL', 'Y');
+INSERT INTO code_generic VALUES(nextval('s_code_generic_pk'), 'SOURCE_STATE', '5', 'SYMBOLS_READ', 'Y');
+INSERT INTO code_generic VALUES(nextval('s_code_generic_pk'), 'SOURCE_STATE', '10', 'RESOURCES_GENERATED', 'Y');
+INSERT INTO code_generic VALUES(nextval('s_code_generic_pk'), 'SOURCE_STATE', '20', 'SAVED_TO_STAGE', 'Y');
+INSERT INTO code_generic VALUES(nextval('s_code_generic_pk'), 'SOURCE_STATE', '30', 'FILTERED', 'Y');
+INSERT INTO code_generic VALUES(nextval('s_code_generic_pk'), 'SOURCE_STATE', '40', 'SAVED_TO_SNAPSHOT', 'Y');
+INSERT INTO code_generic VALUES(nextval('s_code_generic_pk'), 'SOURCE_STATE', '100', 'FAILED', 'Y');
