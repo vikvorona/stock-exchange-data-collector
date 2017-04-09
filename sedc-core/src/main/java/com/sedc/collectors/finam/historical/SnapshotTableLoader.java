795f40705a534ac65348ecf5856a026d2a788f22
Iana_Guseva <Iana_Guseva@epam.com>
2017-04-09 11:50:50 +0300

    add load to ssnapshot query

--- sedc-core/src/main/java/com/sedc/collectors/finam/historical/SnapshotTableLoader.java
+++ sedc-core/src/main/java/com/sedc/collectors/finam/historical/SnapshotTableLoader.java
@@ -19,8 +19,16 @@ public class SnapshotTableLoader implements ItemReader<Object> {
     public Object read() {
         Session session = sessionFactory.openSession();
         session.doWork(connection -> {
-            String query = "INSERT INTO SNAPSHOT_HISTORICAL (per, date, time, open, high, low, close, volume, active_flag) " +
-                    "select s.per, s.date, s.time, s.open, s.high, s.low, s.close, s.volume, s.active_flag from stage_finam_historical s;";
+            String query = "insert into snapshot_historical (scei_id, business_date, sym_id, per, date, time, " +
+                    "open, high, low, close, volume, active_flag) " +
+                    "with source_center_engine_instance as (" +
+                    "  select scei_id, business_date FROM source_center_engine_instance ORDER BY scei_id DESC LIMIT 1" +
+                    " ), stage_finam_historical as (" +
+                    "  select sym_id, per, date, time, open, high, low, close, volume, active_flag from stage_finam_historical" +
+                    " )" +
+                    "select scei.scei_id, scei.business_date, sfh.sym_id, sfh.per, sfh.date, sfh.time, sfh.open, " +
+                    "sfh.high, sfh.low, sfh.close, sfh.volume, sfh.active_flag " +
+                    "from source_center_engine_instance as scei, stage_finam_historical as sfh";
             CallableStatement statement = connection.prepareCall(query);
             statement.executeUpdate();
         });
