bd14570aa448f347f76bd9fe96d03fa75e88d01a
Oleg Shulykov <olegshulyakov@users.noreply.github.com>
2017-04-13 22:35:53 +0300

    SEDC-141: revert query

--- sedc-core/src/main/java/com/sedc/collectors/finam/historical/SnapshotTableLoader.java
+++ sedc-core/src/main/java/com/sedc/collectors/finam/historical/SnapshotTableLoader.java
@@ -3,9 +3,10 @@ package com.sedc.collectors.finam.historical;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.batch.item.ItemReader;
 
-import java.util.Date;
+import java.sql.CallableStatement;
 
 public class SnapshotTableLoader implements ItemReader<Object> {
 
@@ -16,15 +17,23 @@ public class SnapshotTableLoader implements ItemReader<Object> {
     @Override
     public Object read() {
         Session session = sessionFactory.openSession();
-        session.createSQLQuery("" +
-                "INSERT INTO snapshot_historical (scei_id, business_date, sym_id, period, date, time, open, high, low, close, volume)\n" +
-                "SELECT :scei_id, :business_date, sym_id, per, date, time, open, high, low, close, volume\n" +
-                "FROM stage_finam_historical\n" +
-                "WHERE ACTIVE_FLAG = 'Y'")
-                .setLong("scei_id", 1l) //TODO: put value from SourceCenterEngineInstance
-                .setDate("business_date", new Date())
-                .executeUpdate();
-        session.getTransaction().commit();
+        Transaction t = session.getTransaction();
+        t.begin();
+        session.doWork(connection -> {
+            String query = "" +
+                    "insert into snapshot_historical (scei_id, business_date, sym_id, period, date, time, open, high, low, close, volume, active_flag) " +
+                    "with source_center_engine_instance as (" +
+                    "  select scei_id, business_date FROM source_center_engine_instance ORDER BY scei_id DESC LIMIT 1" +
+                    " ), stage_finam_historical as (" +
+                    "  select sym_id, per, date, time, open, high, low, close, volume, active_flag from stage_finam_historical" +
+                    " )" +
+                    "select scei.scei_id, scei.business_date, sfh.sym_id, sfh.per, sfh.date, sfh.time, sfh.open, " +
+                    "sfh.high, sfh.low, sfh.close, sfh.volume, sfh.active_flag " +
+                    "from source_center_engine_instance as scei, stage_finam_historical as sfh";
+            CallableStatement statement = connection.prepareCall(query);
+            statement.executeUpdate();
+        });
+        t.commit();
         session.close();
 
         return null;
