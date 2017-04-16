f5efc61c856a85d9e076c9fdb63f129674e5a56a
Oleg Shulykov <olegshulyakov@users.noreply.github.com>
2017-04-16 07:06:35 +0300

    SEDC-141: fix finam loading

--- sedc-core/src/main/java/com/sedc/collectors/finam/historical/FinamResourceGenerator.java
+++ sedc-core/src/main/java/com/sedc/collectors/finam/historical/FinamResourceGenerator.java
@@ -1,32 +1,57 @@
 package com.sedc.collectors.finam.historical;
 
-import com.sedc.collectors.finam.FinamUtils;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.log4j.Logger;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.UrlResource;
 
-import java.util.Date;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.List;
 
 public class FinamResourceGenerator implements ItemWriter<String> {
 
     private static final Logger LOG = Logger.getLogger(FinamResourceGenerator.class);
+    private static final String HOST_URL = "http://export.finam.ru";
+    private static final String BASE_URL = "/table.csv?d=d&market=1&f=table&e=.csv&dtf=1&tmf=1&MSOR=1&sep=1&sep2=1&datf=1&";
 
-    @Autowired
     private List<UrlResource> urls;
-    @Autowired
-    private String period;
-    @Autowired
-    private Date dateFrom;
-    @Autowired
-    private Date dateTo;
 
+    private String period;
+
+    private LocalDate dateFrom;
+
+    private LocalDate dateTo;
+
+    private static URI buildUrl(String symbol, LocalDate dateFrom, LocalDate dateTo, String period) throws URISyntaxException {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+        URIBuilder uri = new URIBuilder(HOST_URL + BASE_URL);
+
+        uri.addParameter("p", period);
+        uri.addParameter("em", "16842"); //TODO load codes from [http://www.finam.ru/cache/icharts/icharts.js]
+
+        uri.addParameter("df", String.valueOf(dateFrom.getDayOfMonth()));
+        uri.addParameter("mf", String.valueOf(dateFrom.getMonthValue() - 1));
+        uri.addParameter("yf", String.valueOf(dateFrom.getYear()));
+
+        uri.addParameter("dt", String.valueOf(dateTo.getDayOfMonth()));
+        uri.addParameter("mt", String.valueOf(dateTo.getMonthValue() - 1));
+        uri.addParameter("yt", String.valueOf(dateTo.getYear()));
+
+        uri.addParameter("cn", symbol);
+        uri.addParameter("code", symbol);
+
+        return uri.build();
+    }
 
     public void write(List<? extends String> items) throws Exception {
 
         for (String symbol : items) {
-            UrlResource resource = new UrlResource(FinamUtils.buildUrl(symbol, dateFrom, dateTo, period));
+            UrlResource resource = new UrlResource(buildUrl(symbol, dateFrom, dateTo, period));
             LOG.debug("Generated resource: " + resource);
             urls.add(resource);
         }
@@ -40,11 +65,11 @@ public class FinamResourceGenerator implements ItemWriter<String> {
         this.period = period;
     }
 
-    public void setDateFrom(Date dateFrom) {
+    public void setDateFrom(LocalDate dateFrom) {
         this.dateFrom = dateFrom;
     }
 
-    public void setDateTo(Date dateTo) {
+    public void setDateTo(LocalDate dateTo) {
         this.dateTo = dateTo;
     }
 }
