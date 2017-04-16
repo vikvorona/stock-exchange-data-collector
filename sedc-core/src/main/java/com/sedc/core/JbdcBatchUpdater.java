9e950350a7b11a2e39ab080e86d5836f1cb286eb
Oleg Shulykov <olegshulyakov@users.noreply.github.com>
2017-04-16 22:17:14 +0300

    SEDC-143: add stageCleaner

---
+++ sedc-core/src/main/java/com/sedc/core/JbdcBatchUpdater.java
@@ -0,0 +1,33 @@
+package com.sedc.core;
+
+import lombok.Setter;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.batch.item.ItemReader;
+
+public class JbdcBatchUpdater implements ItemReader {
+
+    @Setter
+    private SessionFactory sessionFactory;
+
+    @Setter
+    private String sql;
+
+    @Override
+    public Object read() throws Exception {
+        Session session = sessionFactory.openSession();
+        Transaction t = session.getTransaction();
+        t.begin();
+        try {
+            session.createSQLQuery(sql).executeUpdate();
+            t.commit();
+        } catch (Exception e) {
+            t.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+        return null;
+    }
+}
