14d58920ceee83764113d1f26681816e33cb3e11
Oleg Shulykov <olegshulyakov@users.noreply.github.com>
2017-04-16 19:06:51 +0300

    set parameter region for yahoo jobs

--- sedc-core/src/main/java/com/sedc/collectors/yahoo/historical/YahooHistoricalApplication.java
+++ sedc-core/src/main/java/com/sedc/collectors/yahoo/historical/YahooHistoricalApplication.java
@@ -1,9 +1,19 @@
 package com.sedc.collectors.yahoo.historical;
 
+import com.sedc.Region;
 import org.springframework.batch.core.launch.support.CommandLineJobRunner;
 
 public class YahooHistoricalApplication {
+
     public static void main(String[] args) throws Exception {
-        CommandLineJobRunner.main(new String[]{"spring/batch/jobs/yahoo/yahoo-historical-load-job.xml", "yahooHistoricalLoadJob", "region=NAM"});
+
+        if (args == null || args.length != 1) {
+            System.out.println("USAGE: <region>");
+            return;
+        }
+
+        Region region = Region.valueOf(args[0]);
+
+        CommandLineJobRunner.main(new String[]{"spring/batch/jobs/yahoo/yahoo-historical-load-job.xml", "yahooHistoricalLoadJob", "region=" + region});
     }
 }
--- sedc-core/src/main/java/com/sedc/collectors/yahoo/xchange/YahooXchangeApplication.java
+++ sedc-core/src/main/java/com/sedc/collectors/yahoo/xchange/YahooXchangeApplication.java
@@ -1,13 +1,19 @@
 package com.sedc.collectors.yahoo.xchange;
 
+import com.sedc.Region;
 import org.springframework.batch.core.launch.support.CommandLineJobRunner;
 
-/**
- * Created by SuperOleg on 01.03.2017.
- */
 public class YahooXchangeApplication {
 
     public static void main(String[] args) throws Exception {
-        CommandLineJobRunner.main(new String[]{"spring/batch/jobs/yahoo/yahoo-xchange-load-job.xml", "yahooXchangeLoadJob", "region=FOREX"});
+
+        if (args == null || args.length != 1) {
+            System.out.println("USAGE: <region>");
+            return;
+        }
+
+        Region region = Region.valueOf(args[0]);
+
+        CommandLineJobRunner.main(new String[]{"spring/batch/jobs/yahoo/yahoo-xchange-load-job.xml", "yahooXchangeLoadJob", "region=" + region});
     }
 }
