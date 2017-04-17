67447e10ebbea321f57a527f6e2541203b45762f
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2017-04-17 20:17:14 +0300

    SEDC-143: fix table DDL

--- docs/database/tables/STAGE_YAHOO_HISTORICAL.sql
+++ docs/database/tables/STAGE_YAHOO_HISTORICAL.sql
@@ -1,18 +1,17 @@
 CREATE TABLE STAGE_YAHOO_HISTORICAL(
     SYH_ID              NUMERIC(18) NOT NULL,
-    HASH                NUMERIC(18),
     SYMBOL              VARCHAR(20) NOT NULL,
     SYM_ID              NUMERIC(18),
     PER                 VARCHAR(20) NOT NULL,
     DATE                DATE NOT NULL,
-    OPEN                NUMERIC(18),
-    HIGH                NUMERIC(18),
-    LOW                 NUMERIC(18),
-    CLOSE               NUMERIC(18),
-    VOLUME              NUMERIC(18),
+    OPEN                NUMERIC(18,2),
+    HIGH                NUMERIC(18,2),
+    LOW                 NUMERIC(18,2),
+    CLOSE               NUMERIC(18,2),
+    VOLUME              NUMERIC(18,2),
     ACTIVE_FLAG         CHAR(1) DEFAULT 'Y' NOT NULL CHECK (ACTIVE_FLAG in ('Y','N') ),
     ACTIVE_REASON       VARCHAR(20),
 
-    UNIQUE (SYMBOL, PER, CREATION_TM, ACTIVE_FLAG),
+    UNIQUE (SYMBOL, PER, DATE, ACTIVE_FLAG),
     CONSTRAINT IPK_STAGE_YAHOO_HISTORICAL PRIMARY KEY (SYH_ID)
 );
